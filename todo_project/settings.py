b1e7b414962b5a77ce0762e99caaaa196ccad978
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2025-06-05 12:07:28 +0530

    updateS

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todo-project-prgg.onrender.com']
 
 
